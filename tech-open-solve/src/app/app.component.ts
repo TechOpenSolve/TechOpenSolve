736d6f3c41bc9d16ba71caf56e7a1047b8512d5b
Esther White <monacodelisa@gmail.com>
2024-04-29 22:14:30 +0300

    feat(theme): theming added signals my custom slide toggle with icons

--- tech-open-solve/src/app/app.component.ts
+++ tech-open-solve/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit, PLATFORM_ID, Inject } from "@angular/core";
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, PLATFORM_ID, Inject, inject } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { isPlatformBrowser } from "@angular/common";
 import { DomSanitizer } from "@angular/platform-browser";
@@ -11,6 +11,7 @@ import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { ThemeService } from "./services/theme.service";
 
 
 
@@ -34,6 +35,7 @@ import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 export class AppComponent implements OnInit, OnDestroy {
 	mobileQuery: MediaQueryList;
   switchIcons: any;
+  themeService = inject(ThemeService);
 
   navItems = [
     { name: "Women In Tech", route: "/women-in-tech" },
@@ -62,6 +64,10 @@ export class AppComponent implements OnInit, OnDestroy {
       <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" aria-hidden="true" fill="currentColor" class="mdc-switch__icon mdc-switch__icon--on"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>
       `;
     }
+  }
+
+  toggleTheme() {
+    this.themeService.updateTheme();
   }
 
   ngOnDestroy(): void {
