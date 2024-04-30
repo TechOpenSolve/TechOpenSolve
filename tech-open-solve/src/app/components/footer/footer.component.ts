fb4100b3aa625a57c1d357881b0fb2db5f3a1d07
Esther White <monacodelisa@gmail.com>
2024-04-30 18:18:23 +0300

    feat(social-links): social links component created and added in footer

--- tech-open-solve/src/app/components/footer/footer.component.ts
+++ tech-open-solve/src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialLinksComponent } from '../partials/social-links/social-links.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [SocialLinksComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
