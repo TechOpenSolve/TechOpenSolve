2c83b65695df463b2493e44542c6c511026d60f0
Esther White <monacodelisa@gmail.com>
2024-05-05 11:57:35 +0300

    feat(home): content added to home component

--- tech-open-solve/src/app/components/home/home.component.ts
+++ tech-open-solve/src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatDividerModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
